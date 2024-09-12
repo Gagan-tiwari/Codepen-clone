aafc5e04d6e7762161a1e8bb99bd0bc0379603b7
Gagan <gagantiwari370@gmail.com>
2024-09-12 21:45:32 +0530

    normal changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  build: {
+    cssCodeSplit: true,
+    outDir: "dist",
+    assetsInlineLimit: 4096,
+  },
+  css: {
+    devSourcemap: true,
+  },
+});
